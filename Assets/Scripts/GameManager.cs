7931484053b866f39bc666bc081d9649833d3629
Stephen Mangum <stephenmangum01@gmail.com>
2025-04-29 16:48:44 -0500

    yes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,10 +14,8 @@ public class GameManager : MonoBehaviour
 
     public GameObject ballPrefab;
     public Transform ballSpawnHeight;
-    public float spawnCooldown = 1f; // Time (in seconds) between ball spawns
+    public float spawnCooldown = 1f;
     private float lastSpawnTime = -999f;
-
-
 
     void Awake()
     {
@@ -33,35 +30,22 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
-
     void Start()
     {
-        int savedMoney, savedScore;
-        string equippedBall;
-        SaveManager.LoadProgress(out savedMoney, out savedScore, out equippedBall);
-
-        money = savedMoney;
-        score = savedScore;
-
-        BallManager.instance.EquipBallByName(equippedBall); // You'll implement this in BallManager
-
+        score = 0;
+        money = 5;
         UpdateUI();
     }
 
-
-
     void Update()
     {
-        if (GameManager.shopIsOpen) return;
-        {
-            if (Input.GetMouseButtonDown(0) && Time.time >= lastSpawnTime + spawnCooldown)
-            {
-                TryDropBall();
-                lastSpawnTime = Time.time;
-            }
-        }
+        if (shopIsOpen) return;
 
+        if (Input.GetMouseButtonDown(0) && Time.time >= lastSpawnTime + spawnCooldown)
+        {
+            TryDropBall();
+            lastSpawnTime = Time.time;
+        }
     }
 
     void TryDropBall()
@@ -71,14 +55,12 @@ public class GameManager : MonoBehaviour
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             worldPos = new Vector3(worldPos.x, ballSpawnHeight.position.y, 0);
 
-            // Use equipped ball if available
             GameObject prefabToSpawn = BallManager.instance.currentBall != null
                 ? BallManager.instance.currentBall.prefab
                 : ballPrefab;
 
             Instantiate(prefabToSpawn, worldPos, Quaternion.identity);
             money -= 1;
-            SaveManager.SaveProgress(money, score, BallManager.instance.currentBall.ballName);
             UpdateUI();
         }
         else
@@ -87,28 +69,23 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
     public void AddScore(int amount)
     {
         score += amount;
-        SaveManager.SaveProgress(money, score, BallManager.instance.currentBall.ballName);
         UpdateUI();
     }
 
     public void AddMoney(int amount)
     {
         money += amount;
-        SaveManager.SaveProgress(money, score, BallManager.instance.currentBall.ballName);
         UpdateUI();
     }
-
 
     public void UpdateUI()
     {
         scoreText.text = "Score: " + score;
         moneyText.text = "Money: $" + money;
     }
-
 }
 
 
