707922c39776ee87ef2c55ea53271485a1c949c0
Stephen Mangum <stephenmangum01@gmail.com>
2025-08-15 10:32:57 -0500

    yes

--- Assets/Scripts/BallShopItem.cs
+++ Assets/Scripts/BallShopItem.cs
@@ -7,6 +7,8 @@ public class BallShopItem : MonoBehaviour
     public BallData ballData; // Reference to the BallData script attached to the same GameObject
     public Button purchaseButton;
     public TextMeshProUGUI priceText;
+    public AudioClip purchaseSound;
+    private AudioSource audioSource;
 
     private void Start()
     {
@@ -28,11 +30,19 @@ public class BallShopItem : MonoBehaviour
                 moneyMultiplier = ballData.moneyMultiplier
             };
 
+            // Plays a purchase sound to ensure player bought the ball
+            if (purchaseSound != null)
+                audioSource.PlayOneShot(purchaseSound);
+
             Debug.Log(ballData.ballName + " purchased and equipped!");
+
+            purchaseButton.interactable = false;
         }
         else
         {
             Debug.Log("Not enough money to purchase " + ballData.ballName);
+
+            purchaseButton.interactable = true;
         }
     }
 }
